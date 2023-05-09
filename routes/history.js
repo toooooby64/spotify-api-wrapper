d4c4775144601f90362b9a1633c42196be808360
toooooby64 <jalvarez5667@gmail.com>
2023-05-09 10:56:15 -0700

    created search/:searchTerm end point in search.js

--- routes/history.js
+++ routes/history.js
@@ -2,9 +2,6 @@ const router = require("express").Router();
 const api = require("./api");
 
 router.get("/", async (req, res) => {
-  const test = await api.searchArtist("Kendrick Lamar");
-  res.json(test);
-  console.log(test);
 });
 
 module.exports = router;
--- routes/search.js
+++ routes/search.js
@@ -1,7 +1,12 @@
 const router = require("express").Router();
+const api = require("./api");
 
-router.get("/", (req, res) => {
+router.get("/:searchTerm", async (req, res) => {
   console.log("this is the search endpoint");
+  const search = req.params.searchTerm.replace("-", " ");
+  const test = await api.searchArtist(search);
+  res.json(test);
+  //  console.log(test);
 });
 
 module.exports = router;
